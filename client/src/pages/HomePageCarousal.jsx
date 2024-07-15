b46cdf5a6148a6a96ffa43b806af566e2a532133
preetee-awadh <preetee-awadh@pluralsight.com>
2024-07-15 10:52:06 +0530

    home page design and image addition

--- client/src/pages/HomePageCarousal.jsx
+++ client/src/pages/HomePageCarousal.jsx
@@ -1,39 +1,42 @@
 import Carousel from "react-bootstrap/Carousel";
-//import ExampleCarouselImage from "components/ExampleCarouselImage";
+import Image from "react-bootstrap/Image";
 
 function HomePageCarousal() {
   return (
     <Carousel fade>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="First slide" /> */}
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Image src="/images/intro.jpg" fluid />
+        <Carousel.Caption className="text-dark">
+          <h2>
+            <strong>BookShip</strong>
+          </h2>
+          <p>Connecting the World through Books.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Second slide" /> */}
+        <Image src="/images/bookmission.jpg" fluid />
         <Carousel.Caption>
-          <h3>Mission</h3>
+          <h2>
+            <strong>Mission</strong>
+          </h2>
           <p>
-            "To connect book lovers with the stories and knowledge they seek by
-            providing a personalized, accessible, and engaging reading
-            experience. Our bookstore app offers a vast collection of books,
-            thoughtful recommendations, and a vibrant community where readers
-            can discover, share, and celebrate their love for literature."
+            "To connect book lovers with the stories and knowledge they seek{" "}
+            <br />
+            by providing a personalized, accessible, and engaging reading
+            experience."
           </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        {/* <ExampleCarouselImage text="Third slide" /> */}
+        <Image src="/images/bookvision.jpg" fluid />
         <Carousel.Caption>
-          <h3>Vision</h3>
+          <h2>
+            <strong>Vision</strong>
+          </h2>
           <p>
             "To be the leading digital destination for readers worldwide,
-            revolutionizing the way people discover, access, and enjoy books. We
-            envision a future where our app empowers every reader with the tools
-            and inspiration to explore diverse literary worlds, fostering a
-            global community united by the joy of reading."
+            <br />
+            revolutionizing the way people discover, access, and enjoy books."
           </p>
         </Carousel.Caption>
       </Carousel.Item>
