4501cda7278b150f2543f38742425e1742d30edd
preetee-awadh <preetee-awadh@pluralsight.com>
2024-09-20 14:06:41 +0530

    last changes - changing models names

--- client/src/pages/PaginationOnData.jsx
+++ client/src/pages/PaginationOnData.jsx
@@ -38,13 +38,13 @@ export default function PaginationOnData({
               xmlns="http://www.w3.org/2000/svg"
               width="18"
               height="15"
-              fill="currentColor"
-              viewBox="0 0 15 15"
+              // fill="currentColor"
+              viewBox="0 0 18 15"
             >
               <path
-                fillRule="evenodd"
+                // fillRule="evenodd"
                 d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z"
-                className="fa-secondary"
+                // className="fa-secondary"
               />
             </svg>
           </a>
